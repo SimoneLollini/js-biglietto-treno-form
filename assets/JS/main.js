82b41ebbfb49ad7657fef4d47c7f1b6dd82df23e
SimoneLollini <lollinisimone@gmail.com>
2022-10-07 19:26:34 +0200

    add output for users

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -13,27 +13,32 @@ document.getElementById('genera').addEventListener('click', function () {
     // console.log(userAge);
 
     // calcolare il prezzo totale del biglietto di viaggio
-    const price = km * 0.21
+    const price = km * 1
     const under = document.getElementById('under').checked
     const over = document.getElementById('over').checked
     const maggiorenne = document.getElementById('maggiorenne').checked
 
     if (under == true) {
         discount = 0.20
-
+        document.getElementById("discount").innerHTML = "Sconto Minorenne";
     } else if (over == true) {
         discount = 0.40
+        document.getElementById("discount").innerHTML = "Sconto Over-65";
+
 
     } else {
         discount = 0
-
+        document.getElementById("discount").innerHTML = "Prezzo pieno";
     }
 
-    const finalPrice = price - (price * discount)
-    console.log(finalPrice.toFixed(2));
+    const finalPrice = (price - (price * discount)).toFixed(2)
+    console.log(finalPrice);
+    document.getElementById("ticket_price").innerHTML = finalPrice + " " + "€"
+
+    document.getElementById("codice").innerHTML = Math.floor(Math.random() * 100000);
+    document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 11);
+
+    const name = document.getElementById('name').value;
+    document.getElementById("user_name").innerHTML = " " + name
+    console.log(name);
 });
-
-
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
